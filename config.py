0b3446d293dcbbcfa8969eda377b9a9eeb1dfd2e
ahdkh <aaaddss356@gmail.com>
2024-12-15 00:35:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578"))
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")
-BOT_TOKEN = getenv("BOT_TOKEN", "5385370603:AAHjSbZFL1OLl_uOm3v8lS0iKSYhRJS_bKs")
-SESSION_NAME = getenv("SESSION_NAME", "AgAGFRW8AN9biZhAIB_X7rubbTCwNtc48oa-geXlaVsQbV9M1emAEV_iGPw_W-lNP1HGbOuQgF9NAUIxlp8-6rgN16-7Upuhst2wCxewyx0VP7Cxb5k_9EXap6_xeU2v4So9RZMt95XYxhGc4hWO_ikd9P46iHLMjXW8GjQAnGvY-bIrM6XqIXF1yeC3vixEa8knOHGkedZU3FYyLhEILy8TV6eH5jOGvsyw5o7xFElb2zZId_vlYO--B3xSWIMUR6zyH8wozehe9tIn0jZptYjY1rqWOMoYS9wtqvBThVazL60nrgL-KdikJC9beDJLLR0C-wzuAtARPCZ-JK6oG-gVAAAAAUM3evYA")
+API_ID = int(getenv("API_ID", "10035836"))
+API_HASH = getenv("API_HASH", "2c782854a7292db305eb6c0be74cc28a")
+BOT_TOKEN = getenv("BOT_TOKEN", "8068753850:AAHY7h68xwjt6G0IS1ynrpWyiDP2MV-BsDw")
+SESSION_NAME = getenv("SESSION_NAME", "1ApWapzMBuxLk0RaKb6YHWqIbggdmhk-Q4I9pJbx8d9CY7SmhFN6pcNuf3yEqJxeCU_S45eedLWtjL64Rc9PVoE5t6AtZ-o6RLeLfksmi5Jtde3_SrqzqMDj01GdjdEP9BLklYCHib4WoCH3w0xrS9znBDUGjpQZt8wvHRO_eNFONB7Tk6UXrOsgbHij7srv0u2gpHpZXrUZmUT-rZPf-qlOdRXj8twbl84EoCgAlMv3qOAoiM43LwTt1MJ_I3_LKPsJPxoLWugawWNxRQwkv9QYyDgl2-utKpbEFm83JCHjyKIR8S2tgWg8q84IAQqD2leGNEndvv7sGoXsmyu04NZcYzslrNho=")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "vvvvvvvvvvvvvvvvvvvvvvvvvvvvg")
 ALIVE_NAME = getenv("ALIVE_NAME", "song")
-BOT_USERNAME = getenv("BOT_USERNAME", "W7MBoT")
+BOT_USERNAME = getenv("BOT_USERNAME", "eb_7Bot")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/STKR2/2004")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "LINKASKR")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "LINKASKR")
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
